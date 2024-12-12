13d67c721c4f391485b3c555ca74990d2b3585d3
Muhammad Rifqil Khanif <115907124+Rifqil-Khanif@users.noreply.github.com>
2024-12-12 23:20:06 +0700

    Create userRoute.js

---
+++ routes/userRoute.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+
+// tambahkan route untuk user di sini
+
+module.exports = router;
